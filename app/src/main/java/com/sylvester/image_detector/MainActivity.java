2692cae369f04c9447392f3a40ee1e3f78ce391d
sylvester ranjith <sylvesterranjithfrancis@gmail.com>
2019-12-07 16:30:01 +0530

    application code - first commit

---
+++ app/src/main/java/com/sylvester/image_detector/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sylvester.image_detector;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
